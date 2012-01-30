6895037c9573e3c912772ccdf1cae57361d39a8e
yusimoncn@gmail.com <yusimoncn@gmail.com@4b8541b3-ff67-2e55-8809-c0163d75dc9a>
2012-01-30 02:38:11 +0000

    added LicenceBean in the rifcs registration implementation

---
+++ trunk/server/src/java/edu/monash/merc/dto/LicenceBean.java
@@ -0,0 +1,74 @@
+package edu.monash.merc.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author: Simon Yu
+ * @email: Xiaoming.Yu@monash.edu
+ *
+ * Date: 30/01/12
+ * Time: 12:05 PM
+ * @version: 1.0
+ */
+public class LicenceBean implements Serializable {
+
+    private long id;
+
+    private String licenceType;
+
+    private String commercial;
+
+    private String derivatives;
+
+    private String jurisdiction;
+
+    private String licenceContents;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLicenceType() {
+        return licenceType;
+    }
+
+    public void setLicenceType(String licenceType) {
+        this.licenceType = licenceType;
+    }
+
+    public String getCommercial() {
+        return commercial;
+    }
+
+    public void setCommercial(String commercial) {
+        this.commercial = commercial;
+    }
+
+    public String getDerivatives() {
+        return derivatives;
+    }
+
+    public void setDerivatives(String derivatives) {
+        this.derivatives = derivatives;
+    }
+
+    public String getJurisdiction() {
+        return jurisdiction;
+    }
+
+    public void setJurisdiction(String jurisdiction) {
+        this.jurisdiction = jurisdiction;
+    }
+
+    public String getLicenceContents() {
+        return licenceContents;
+    }
+
+    public void setLicenceContents(String licenceContents) {
+        this.licenceContents = licenceContents;
+    }
+}
